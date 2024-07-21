69d9990fb7d924b668de09118a255a682c31bc47
Zaire McAllister <ZaireM21@gmail.com>
2024-07-21 17:27:09 -0400

    Initial Commit

---
+++ PetCheck/PetCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetCheckApp.swift
+//  PetCheck
+//
+//  Created by Zaire McAllister on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PetCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
