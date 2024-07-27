3cb9892b750913431ea3141fd53fe6d58dd7a3b6
Zaire McAllister <ZaireM21@gmail.com>
2024-07-26 22:11:51 -0400

    chore: sqlite database additions

--- PetCheck/LocalDatabase/LocalDatabase.swift
+++ PetCheck/LocalDatabase/LocalDatabase.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 import GRDB
 
 struct LocalDatabase {
@@ -19,5 +20,46 @@ struct LocalDatabase {
     
     var reader: DatabaseReader {
         writer
+    }
+}
+
+
+// MARK: - Writes
+extension LocalDatabase {
+    func createUser(_ user: User) async throws {
+        try await writer.write { db in
+            try user.insert(db)
+        }
+    }
+    
+    func createPet(_ pet: Pet) async throws {
+        try await writer.write { db in
+            try pet.insert(db)
+        }
+    }
+}
+
+// MARK: - Observe
+
+extension LocalDatabase {
+    func  observeUser() -> AnyPublisher<[User], Error> {
+        
+        let observation = ValueObservation.tracking { db in
+            return try User.fetchAll(db)
+        }
+        
+        let publisher = observation.publisher(in: reader)
+        return publisher.eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Reads
+
+extension LocalDatabase {
+    func getPetsForUser(_ user: User) async throws -> [Pet] {
+        try await reader.read { db in
+            let pets = try user.pets.fetchAll(db)
+            return pets
+        }
     }
 }
---
+++ PetCheck/Models/PetCheckModels.swift
@@ -0,0 +1,39 @@
+//
+//  PetCheckModels.swift
+//  PetCheck
+//
+//  Created by Zaire McAllister on 7/26/24.
+//
+
+import Foundation
+import GRDB
+
+struct User: Identifiable, Codable {
+    var id: String
+    var name: String
+    var email: String
+}
+
+extension User: EncodableRecord, FetchableRecord {}
+
+extension User: TableRecord {
+    static let pets = hasMany(Pet.self)
+    
+    var pets: QueryInterfaceRequest<Pet> {
+        request(for: User.pets)
+    }
+}
+
+extension User: PersistableRecord {}
+
+
+struct Pet: Identifiable, Codable {
+    var id: Int64?
+    var name: String
+    var dateOfBirth: Date
+    var breed: String
+    var userId: String
+}
+
+extension Pet: TableRecord, FetchableRecord, EncodableRecord {}
+extension Pet: PersistableRecord {}
